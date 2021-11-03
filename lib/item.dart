57e7189f63e2b0005c70c162a338da284300c234
Farhad Misirli <farhadmisirli@gmail.com>
2021-11-03 18:16:24 +0400

    Completed

---
+++ lib/item.dart
@@ -0,0 +1,9 @@
+class Item {
+  String value = '';
+  bool isChecked = false;
+
+  Item(String value, bool isChecked) {
+    this.value = value;
+    this.isChecked = isChecked;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:todo/item.dart';
 
-void main() => runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: TodoApp()
-));
+void main() => runApp(TodoApp());
+
+class Test extends StatelessWidget {
+  const Test({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Hello"),
+          ),
+          body: Column(
+            children: [
+              TextField(
+
+              ),
+
+              Expanded(
+                flex: 1,
+                  child: Container(
+                    width: double.infinity,
+                    height: 100.0,
+                    color: Colors.red,
+                    child: ListView(
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          height: 200.0,
+                          color: Colors.green,
+                          margin: EdgeInsets.all(10),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          height: 200.0,
+                          color: Colors.green,
+                          margin: EdgeInsets.all(10),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          height: 200.0,
+                          color: Colors.green,
+                          margin: EdgeInsets.all(10),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          height: 200.0,
+                          color: Colors.green,
+                          margin: EdgeInsets.all(10),
+                        ),
+                      ],
+                    ),
+                  )
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
 
 class TodoApp extends StatefulWidget {
   const TodoApp({Key? key}) : super(key: key);
@@ -13,9 +70,12 @@ class TodoApp extends StatefulWidget {
 }
 
 class _TodoAppState extends State<TodoApp> {
-  bool isChecked = false;
-  List taskList = [];
+  // bool isChecked = false;
+  List<Item> taskList = [];
   final myController = TextEditingController();
+  var _dark_theme = false;
+  var _show_finished = false;
+
 
   @override
   Widget build(BuildContext context) {
@@ -32,94 +92,201 @@ class _TodoAppState extends State<TodoApp> {
       return Colors.green;
     }
 
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      appBar: AppBar(
-        title: Text(
-          "DevCave Todo",
-          style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold
-          ),
-        ),
-        centerTitle: false,
-        backgroundColor: Colors.white,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+      brightness: Brightness.light,
+      /* light theme settings */
       ),
-
-      body: Column(
-        children: [
-          SizedBox(height: 10.0),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: myController,
-              maxLength: 500,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Write Task...',
-                fillColor: Colors.grey[200],
-                filled: true,
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.send),
-                  onPressed: (() {
-                    setState(() {
-                      taskList.add(myController.value.text);
-                      myController.clear();
-                    });
-                  }),
-                ),
-
-              ),
-              onSubmitted: (value) {
-                setState(() {
-                  taskList.add(value);
-                  myController.clear();
-                });
-              },
+      darkTheme: ThemeData(
+      brightness: Brightness.dark,
+      /* dark theme settings */
+      ),
+      themeMode: _dark_theme ? ThemeMode.dark : ThemeMode.light,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "DevCave Todo",
+            style: TextStyle(
+                fontWeight: FontWeight.bold
             ),
           ),
-          SizedBox(height: 10.0),
-          ListView(
-              shrinkWrap: true,
-              padding: const EdgeInsets.all(8),
-              children: taskList.asMap().entries.map((item) => Card(
-                shadowColor: Colors.grey[600],
-                child: ListTile(
-                  leading: Checkbox(
-                    shape: CircleBorder(),
-                    checkColor: Colors.white,
-                    fillColor: MaterialStateProperty.resolveWith(getColor),
-                    value: isChecked,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        isChecked = value!;
-                      });
-                    },
+          centerTitle: false,
+          actions: <Widget>[
+            IconButton(
+              padding: EdgeInsets.fromLTRB(0, 0, 20.0, 0),
+              icon: Icon(
+                Icons.wb_sunny_outlined,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                // do something
+                setState(() {
+                  _dark_theme = _dark_theme ? false : true;
+                });
+              },
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            SizedBox(height: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: myController,
+                maxLength: 500,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Write Task...',
+                  // fillColor: Colors.grey[200],
+                  filled: true,
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.send),
+                    onPressed: (() {
+                      var _input_data = myController.value.text.trim();
+                      if(_input_data.isNotEmpty){
+                        setState(() {
+                          taskList.insert(0,new Item(_input_data, false));
+                          myController.clear();
+                        });
+                      }
+                    }),
                   ),
-                  subtitle: Text(
-                    '${item.value}',
-                    style: TextStyle(
-                        color: Colors.grey[700],
-                        fontSize: 18.0
+
+                ),
+                onSubmitted: (value) {
+                  var _input_data = myController.value.text.trim();
+                  if(_input_data.isNotEmpty) {
+                    setState(() {
+                      taskList.insert(0,new Item(myController.value.text, false));
+                      myController.clear();
+                    });
+                  }
+                },
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Container(
+              padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+              width: double.infinity,
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton.icon(
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.fromLTRB(10, 0, 13, 0),
+
+                      shape:RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24.0),
+                        side: BorderSide(
+                            color: !_show_finished ? Colors.green : Colors.black54
+                        )
+                      ),
+                    ),
+                    onPressed: () => {
+                      setState(() {
+                        _show_finished = _show_finished ? false : true;
+                      })
+                    },
+                    label: Text(
+                      (!_show_finished ? 'Show' : 'Hide') + ' Finished',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: !_show_finished ? Colors.green : Colors.black54
+                      ),
+                    ),
+                    icon: Icon(
+                        Icons.check_circle,
+                        color: !_show_finished ? Colors.green : Colors.black54
                     ),
                   ),
-                  trailing: IconButton(
-                    onPressed: () {
+
+                  SizedBox(width: 10,),
+                  TextButton.icon(
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.fromLTRB(10, 0, 13, 0),
+                      shape:RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24.0),
+                          side: BorderSide(color: Colors.red)
+                      ),
+                    ),
+                    onPressed: () => {
                       setState(() {
-                        taskList.removeAt(item.key);
-                      });
+                        taskList.clear();
+                      })
                     },
-                    icon: Icon(Icons.remove_circle),
-                    color: Colors.red,
-
+                    label: Text(
+                      'Delete all',
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.red
+                      ),
+                    ),
+                    icon: Icon(Icons.remove, color: Colors.red,),
                   ),
-                  isThreeLine: true,
-                ),
-              )).toList()
-          ),
-        ],
-      ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Expanded(
+              flex: 1,
+              child: ListView(
+                  // physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.all(8),
+                  children: taskList.where((element) => _show_finished ? true :  !element.isChecked  ).toList().asMap().entries.map((item)
+                  {
+                    return Card(
+                      shadowColor: Colors.grey[600],
+                      child: ListTile(
+                        leading: Checkbox(
+                          shape: CircleBorder(),
+                          checkColor: Colors.white,
+                          fillColor: MaterialStateProperty.resolveWith(getColor),
+                          value: item.value.isChecked,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              print(item.value.value);
+                              item.value.isChecked = value!;
+                            });
+                          },
+                        ),
+                        subtitle: Text(
 
+                          '${item.value.value}',
+                          style: TextStyle(
+                            // color: Colors.grey[700],
+                              fontSize: 18.0,
+                              decoration: item.value.isChecked
+                                  ? TextDecoration.lineThrough
+                                  : TextDecoration.none
+                          ),
+                        ),
+
+
+                        trailing: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              taskList.removeAt(item.key);
+                            });
+                          },
+                          icon: Icon(Icons.remove_circle),
+                          color: Colors.red,
+
+                        ),
+                        isThreeLine: true,
+                      ),
+                    );
+
+                  }).toList()
+              ),
+            ),
+          ],
+        ),
+
+      )
     );
   }
 }
